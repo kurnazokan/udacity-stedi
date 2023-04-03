3e8d647b3de5266b0cc9a8a4eda024df2d723cc9
OkanK <118965732+kurnazokan@users.noreply.github.com>
2023-04-03 10:52:14 +0300

    Update trainer_trusted_to_curated.py

--- trainer_trusted_to_curated.py
+++ trainer_trusted_to_curated.py
@@ -14,13 +14,13 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 # Script generated for node Step Trainer Trusted
-# StepTrainerTrusted_node1676402494135 ??
-# JoinCustomerandStepTrainer_node1676402624725 ??
+# StepTrainerTrusted_node1676402404135 ??
+# JoinCustomerandStepTrainer_node167643624725 ??
 
-StepTrainerTrusted_node1676402494135 = glueContext.create_dynamic_frame.from_catalog(
+StepTrainerTrusted_node1676402404135 = glueContext.create_dynamic_frame.from_catalog(
     database="project",
     table_name="step_trainer_trusted",
-    transformation_ctx="StepTrainerTrusted_node1676402494135",
+    transformation_ctx="StepTrainerTrusted_node1676402404135",
 )
 
 # Script generated for node Accelerometer Trusted
@@ -31,27 +31,27 @@ AccelerometerTrusted_node1 = glueContext.create_dynamic_frame.from_catalog(
 )
 
 # Script generated for node Join Customer and Step Trainer
-JoinCustomerandStepTrainer_node1676402624725 = Join.apply(
+JoinCustomerandStepTrainer_node167643624725 = Join.apply(
     frame1=AccelerometerTrusted_node1,
-    frame2=StepTrainerTrusted_node1676402494135,
+    frame2=StepTrainerTrusted_node1676402404135,
     keys1=["timestamp"],
     keys2=["sensorreadingtime"],
-    transformation_ctx="JoinCustomerandStepTrainer_node1676402624725",
+    transformation_ctx="JoinCustomerandStepTrainer_node167643624725",
 )
 
 # Script generated for node Drop Fields
-DropFields_node1676402768067 = DropFields.apply(
-    frame=JoinCustomerandStepTrainer_node1676402624725,
+DropFields_node1676302768067 = DropFields.apply(
+    frame=JoinCustomerandStepTrainer_node167643624725,
     paths=["user"],
-    transformation_ctx="DropFields_node1676402768067",
+    transformation_ctx="DropFields_node1676302768067",
 )
 
 # Script generated for node Step Trainer Trusted
-StepTrainerTrusted_node1676578616395 = glueContext.write_dynamic_frame.from_catalog(
-    frame=DropFields_node1676402768067,
+StepTrainerTrusted_node1676078606395 = glueContext.write_dynamic_frame.from_catalog(
+    frame=DropFields_node1676302768067,
     database="project",
     table_name="machine_learning_curated",
-    transformation_ctx="StepTrainerTrusted_node1676578616395",
+    transformation_ctx="StepTrainerTrusted_node1676078606395",
 )
 
 job.commit()
